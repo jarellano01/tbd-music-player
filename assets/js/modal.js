14cb7f9dcb5291ebeb615437f5789629752a6551
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-08-20 15:06:00 -0700

    Updated FB function to include song title

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,32 +5,21 @@ $("#trigger-modal").on('click', function() {
 });
 
 $("#submit-song").on('click', function() {
-
-
-
     var name = $('#nameinput').val().trim();
     var dedication = $('#dedicateinput').val().trim();
-    // var title= $("#youTitle").val();
+    var title= $("#youTitle").val();
     var youID= $("#youtubeID").val();
-    addVideoToDatabase(youID, name, dedication);
+    addVideoToDatabase(youID, name, dedication, title);
 
-
+    $('#nameinput').val("");
+    $('#dedicateinput').val('');
 
 })
 
+
 function generateModal(youTitle, youtubeID) {
-    // add hidden youtube shit
-    // var title from hidden value. 
-    // var youtubeID
     $("#youTitle").val("adele");
     $("#youtubeID").val("858420");
 
-    console.log(youtubeID);
-    addVideoToDatabase("Fde24313", "Jonathan", "he is awesome");
-
-
-
-
     $("#modal-id").modal()
-
 }
