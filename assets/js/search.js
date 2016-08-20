5124d376acff20e68bfb143926575621d898a178
Ramiro Lopez <lopezdiaz.ramiro@gmail.com>
2016-08-20 10:11:29 -0700

    Disregard these changes

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,24 +1,57 @@
-$(function() {
+$('#search-button').on('click', function() {
+    var searchTerm = $('#query').val()
 
-    function findYouTube(searchTerm) {
-        var video = searchTerm;
-        var videoArray;
-        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=" + video + "&key=AIzaSyCmhc9fABzn9P5tCslboXz2AcF12NH38oM";
-        console.log(queryURL);
-        
-        $.ajax({
-            url: queryURL,
-            method: 'GET',
-        }).done(function(response) {
-            SetArray(response.items);
-        });
+    videoArray = findYouTube(searchTerm);
 
-    }
-
-    function SetArray(videoArray){
-        console.log(videoArray);
-    }
-
-    findYouTube();
+    console.log(videoArray);
 
 })
+
+function findYouTube(searchTerm) {
+    var video = searchTerm;
+    var videoArray;
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=" + video + "&key=AIzaSyCmhc9fABzn9P5tCslboXz2AcF12NH38oM";
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function(response) {
+
+        return response.items;
+    });
+
+}
+
+
+
+// function search() {
+
+// 	console.log('Search Started');
+//   var apiKey = 'AIzaSyCr7L91URLBfmHfXeUiKPnUbmL0s9gikSY';
+// 	var q = $('#query').val();
+
+//     gapi.client.setApiKey(apiKey);
+//     gapi.client.load('youtube', 'v3', function() {
+//         isLoad = true;
+//     }); 
+// 	console.log('Search Request');
+
+//     request = gapi.client.youtube.search.list({
+// 		q: q,
+//         part: 'id, snippet',
+//         type: 'video',
+//         order: 'date'
+//      });
+
+
+//   request.execute(function(response) {
+//     var str = JSON.stringify(response.result);
+//     var youtubeId = response.items[0].id.videoId
+//     $('#search-container').html('<iframe width="420" height="345" src="https://www.youtube.com/embed/'+youtubeId+ '"></iframe>');
+//     console.log(response);
+//   });
+
+// }
+
+
+
