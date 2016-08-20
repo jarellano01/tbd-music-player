4f37ad98fcf98ac08b06c3d4f3616cd4937a2968
Hannah Marlin <hmmarlin@gmail.com>
2016-08-20 13:51:49 -0700

    working on modal functionality

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,24 +1,15 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDL2knhBw12eUVOv1t57IY-sFScv5rYwrQ",
-    authDomain: "tbd-music-player.firebaseapp.com",
-    databaseURL: "https://tbd-music-player.firebaseio.com",
-    storageBucket: "tbd-music-player.appspot.com",
-  };
-  firebase.initializeApp(config);
-
-  // append data from the Firebase object to the admin panel
 
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   	console.log(childSnapshot);
 
-  	 // var songName = childSnapshot.val().song;
-    //  var artistName = childSnapshot.val().artist;
-    //  var yourName = childSnapshot.val().userName;
-    //  var dedicationName = childSnapshot.val().dedication;
-    //  var songLengthSec = childSnapshot.val().songLength;
+  	 var songName = childSnapshot.val().song;
+     var artistName = childSnapshot.val().artist;
+     var yourName = childSnapshot.val().userName;
+     var dedicationName = childSnapshot.val().dedication;
+     var songLengthSec = childSnapshot.val().songLength;
 
      $("#adminQueue > tbody").append("<tr><td>" + songName + "</td><td>" + artistName + "</td><td>" + yourName + "</td><td>" + dedicationName + "</td><td>" + songLengthSec + "</td></tr>");
 
   }
+
 
---
+++ assets/js/modal.js
@@ -0,0 +1,29 @@
+$("#trigger-modal").on('click', function() {
+            // trigger modal
+            generateModal("hello", "281101");
+
+            });
+
+$("#submit-song").on('click', function (){
+
+
+
+  var song = $(this).id
+            var name = $('#nameinput').val().trim();
+            var dedication = $('#dedicateinput').val().trim();
+
+
+
+})
+
+function generateModal(title, youtubeID) {
+  // add hidden youtube shit
+  // var title from hidden value. 
+  // var youtubeID
+
+
+
+
+$("#modal-id").modal()
+
+}
