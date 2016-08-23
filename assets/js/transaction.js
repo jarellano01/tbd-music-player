6fff9ee73c92c85e79f3e8fdc8b910fca1d22e11
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-08-22 17:17:28 -0700

    Completed basic logic for transaction additions into database

--- assets/js/transaction.js
+++ assets/js/transaction.js
@@ -8,48 +8,58 @@
   firebase.initializeApp(config);
 
   // var database = firebase.database();
-  var adaRankRef = firebase.database().ref('itemsInQueue');
-  adaRankRef.transaction(function(currentData) {
-      // If users/ada/rank has never been set, currentRank will be `null`.
-      if(currentData === null){
-        return currentData;
-      }
-      else{
-        var numObjects = Object.keys(currentData).length;
-        console.log(Object.keys(currentData).length);
-        adaRankRef.push({queueId: numObjects, Name: "Jonathan", id: "1242332", dedication: "shoutout"});
-        
+  function addRecord() {
+      var adaRankRef = firebase.database().ref('itemsInQueue');
+      adaRankRef.transaction(function(currentData) {
+          // If users/ada/rank has never been set, currentRank will be `null`.
+          if (currentData === null) {
+              return currentData;
+          } else {
+              var numObjects = Object.keys(currentData).length;
+              console.log(Object.keys(currentData).length);
+              currentData[numObjects] = {queueID:numObjects, Name: "Jonathan", id: "1242332", dedication: "shoutout"};
+              currentData.noItems = numObjects;
+              return currentData;
+          }
+      });
+  }
 
-        return;
-      }
-      
-  });
-
-
-// var adaRankRef = firebase.database().ref('users/ada/rank');
-// adaRankRef.transaction(function(currentRank) {
-//   // If users/ada/rank has never been set, currentRank will be `null`.
-//   console.log(currentRank);
-//   return currentRank + 1;
-// });
-// var adaRef = firebase.database().ref('users/ada');
-// adaRef.transaction(function(currentData) {
-//   console.log(currentData);
-//   if (currentData === null) {
-//     return { name: { first: 'Ada', last: 'Lovelace' } };
-//   } else {
-//     console.log('User ada already exists.');
-//     return; // Abort the transaction.
-//   }
-// }, function(error, committed, snapshot) {
-//   if (error) {
-//     console.log('Transaction failed abnormally!', error);
-//   } else if (!committed) {
-//     console.log('We aborted the transaction (because ada already exists).');
-//   } else {
-//     console.log('User ada added!');
-//   }
-//   console.log("Ada's data: ", snapshot.val());
-// });
+  addRecord();
+  addRecord();
+  addRecord();
+  addRecord();
+  addRecord();
+  addRecord();
+  addRecord();
+  addRecord();
+  addRecord();
+  addRecord();
 
 
+
+
+  // var adaRankRef = firebase.database().ref('users/ada/rank');
+  // adaRankRef.transaction(function(currentRank) {
+  //   // If users/ada/rank has never been set, currentRank will be `null`.
+  //   console.log(currentRank);
+  //   return currentRank + 1;
+  // });
+  // var adaRef = firebase.database().ref('users/ada');
+  // adaRef.transaction(function(currentData) {
+  //   console.log(currentData);
+  //   if (currentData === null) {
+  //     return { name: { first: 'Ada', last: 'Lovelace' } };
+  //   } else {
+  //     console.log('User ada already exists.');
+  //     return; // Abort the transaction.
+  //   }
+  // }, function(error, committed, snapshot) {
+  //   if (error) {
+  //     console.log('Transaction failed abnormally!', error);
+  //   } else if (!committed) {
+  //     console.log('We aborted the transaction (because ada already exists).');
+  //   } else {
+  //     console.log('User ada added!');
+  //   }
+  //   console.log("Ada's data: ", snapshot.val());
+  // });
