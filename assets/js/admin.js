a3fdc7b9741055318e6a7043cb1be066dc3d1938
Hannah Marlin <hmmarlin@gmail.com>
2016-08-22 19:21:53 -0700

    highlight queue functionality, add modify buttons, some CSS changes

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,16 +1,19 @@
-function getFromDatabase(queueArray){
-	generateTable(queueArray)
+function getFromDatabase(queueArray) {
+    generateTable(queueArray)
 }
 
 function generateTable(queueArray) {
     $("#queue").empty();
     jQuery.each(queueArray, function(i, queueItem) {
         var newRow = $("<tr>");
+        newRow.attr('class', 'selector')
+
+
         var tdTitle = $("<td>");
         tdTitle.text(queueItem.name);
         console.log(queueItem);
 
-        var tdName = $("<td> ");
+        var tdName = $("<td>");
         tdName.text(queueItem.songTitle);
 
         var tdDedication = $("<td>");
@@ -19,5 +22,29 @@ function generateTable(queueArray) {
         newRow.append(tdTitle).append(tdName).append(tdDedication);
 
         $("#queue").append(newRow);
+        // test the ability to grab item from queue list. check console.log
+
+        $(".selector").on('click', function() {
+           
+           var testing = $(this).queueItem
+           console.log(testing);
+           selectRow(this);
+
+        });
+
+
     });
+
+function selectRow(selectedRow){
+
+    $(".selector").attr("class", "selector");
+    $(selectedRow).attr("class", "selector active");
+}
+
+$("#delete-song").click(function(){
+    console.log($(".active"))
+})
+
+
+// SELECT ROW FUNCTION 
 }
