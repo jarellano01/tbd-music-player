b5a853519788dcc363f3ca79136b908f4f34f6ef
Hannah Marlin <hmmarlin@gmail.com>
2016-08-22 20:57:34 -0700

    add functionality to recognize items before and after on queue list

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -16,36 +16,44 @@ function generateTable(queueArray) {
         var tdName = $("<td> ");
         tdName.text(queueItem.title);
 
-        newRow.attr("data-id", queueItem.itemId);
+        newRow.attr("data-id", queueArray[i].itemId);
+
+        if(i !==0)
+            newRow.attr("data-before", queueArray[i-1].itemId);
+
+        newRow.attr("data-after", queueArray[i+1].itemId);
+
         var tdDedication = $("<td>");
         tdDedication.text(queueItem.dedication);
 
         newRow.append(tdTitle).append(tdName).append(tdDedication);
+     
 
         $("#queue").append(newRow);
         // test the ability to grab item from queue list. check console.log
 
         $(".selector").on('click', function() {
-           
-           var testing = $(this).queueItem
-           console.log(testing);
-           selectRow(this);
+
+            var testing = $(this).queueItem
+            console.log(testing);
+            selectRow(this);
+
 
         });
 
 
     });
 
-function selectRow(selectedRow){
+    function selectRow(selectedRow) {
 
-    $(".selector").attr("class", "selector");
-    $(selectedRow).attr("class", "selector active");
-}
+        $(".selector").attr("class", "selector");
+        $(selectedRow).attr("class", "selector active");
+    }
 
-$("#delete-song").click(function(){
-    console.log($(".active"))
-})
+    $("#delete-song").click(function() {
+        console.log($(".active"))
+    })
 
 
-// SELECT ROW FUNCTION 
+    // SELECT ROW FUNCTION 
 }
